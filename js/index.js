9a24354a15164eb54f9440211cbc4f0b94078206
dsds <dsds>
2021-08-13 22:59:40 +0300

    adding files

---
+++ js/index.js
@@ -0,0 +1,61 @@
+'use strict';
+$(document).ready(() => {
+    const $btnHamburger = $('.hamburger');
+
+   $('.user-basket').click((event) => {
+    event.preventDefault();
+
+   });
+   
+    $btnHamburger.click((elem, classN = 'active') => {
+        event.stopPropagation();
+        $btnHamburger.toggleClass(classN);
+        $('#hamburgerNav').toggleClass(classN);
+        $('.header__nav').toggleClass(classN);
+        $('body').click(() => {
+            $btnHamburger.removeClass(classN);
+            $('#hamburgerNav').removeClass(classN);
+            $('.header__nav').removeClass(classN);
+        });
+    });
+    // window.addEventListener('click', (event) => {
+    //     const e = event.target;
+    //     if (e != btnHamburger) {
+    //         return
+    //     } else {
+    //         const hamburgerNav = document.getElementById('hamburgerNav');
+    //         toggleClass(btnHamburger);
+    //         toggleClass(hamburgerNav);
+    //     };
+    // });
+    if ($('.top-banner')) {
+        const setDots = () => $(".top-banner .owl-dots").removeClass('disabled');
+        $('.top-banner').owlCarousel({
+            loop: true,
+            margin: 10,
+            nav: true,
+            items: 1,
+            dots: true,
+            nav: false,
+            autoHeight: true,
+            onInitialized: setDots,
+            onChanged: setDots
+        });
+    };
+    if($('.review-slider')) {
+        const setBtns = () => $(".review-slider .owl-nav").removeClass('disabled');
+        $('.review-slider').owlCarousel({
+            loop: true,
+            margin: 10,
+            items: 1,
+            navText: [],
+            onInitialized: setBtns,
+            onChanged: setBtns
+        });
+    }
+    $('.b-collapse__header').click((e) => {
+        e = event.target;
+        $(e).closest('.b-collapse').toggleClass('collapsed').children('.b-collapse__body').slideToggle(200);
+        // $(e).siblings($('.b-collapse__body')).slideToggle(300)
+    });
+});
