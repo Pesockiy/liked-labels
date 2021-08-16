c775ffe14ef81751e0b2c7cbbda64ccfd7c0df85
Pesockiy <pesockiy.timur>
2021-08-16 16:33:13 +0300

    adding styles

--- js/index.js
+++ js/index.js
@@ -6,41 +6,30 @@ $(document).ready(() => {
         event.preventDefault();
     });
 
+    // перемещение блока 
     if (window.innerWidth <= 758) {
         const $hamburgerNab = $('#hamburgerNav').clone();
         $('#hamburgerNav').remove();
         $('.nav.header__nav').prepend($hamburgerNab);
     };
 
+    $('body').click(() => {
+        $btnHamburger.removeClass('active');
+        $('#hamburgerNav').removeClass('active');
+        $('.header__nav').removeClass('active');
+        if (window.innerWidth <= 758) {
+            if (document.querySelector('.hamburger').classList.contains('active')) {
+                console.log('120');
+                $('body').addClass('ov-hidden');
+            };
+        };
+    });
     $btnHamburger.click((elem, classN = 'active') => {
         event.stopPropagation();
         $btnHamburger.toggleClass(classN);
         $('#hamburgerNav').toggleClass(classN);
         $('.header__nav').toggleClass(classN);
-        $('body').click(() => {
-            $btnHamburger.removeClass(classN);
-            $('#hamburgerNav').removeClass(classN);
-            $('.header__nav').removeClass(classN);
-            if (window.width() <= 758) {
-                if($btnHamburger.contains(classN)) {
-                    $('body').addClass('ov-hidden');
-                } else {
-                    console.log('sdfdsfsd')
-                    $('body').removeClass('ov-hidden');
-                };
-            };
-        });
     });
-    // window.addEventListener('click', (event) => {
-    //     const e = event.target;
-    //     if (e != btnHamburger) {
-    //         return
-    //     } else {
-    //         const hamburgerNav = document.getElementById('hamburgerNav');
-    //         toggleClass(btnHamburger);
-    //         toggleClass(hamburgerNav);
-    //     };
-    // });
     if ($('.top-banner')) {
         const setDots = () => $(".top-banner .owl-dots").removeClass('disabled');
         $('.top-banner').owlCarousel({
@@ -69,6 +58,5 @@ $(document).ready(() => {
     $('.b-collapse__header').click((e) => {
         e = event.target;
         $(e).closest('.b-collapse').toggleClass('collapsed').children('.b-collapse__body').slideToggle(200);
-        // $(e).siblings($('.b-collapse__body')).slideToggle(300)
     });
 });
