f491a7eccaec7431013113ec5abaebdf99798b89
Pesockiy <pesockiy.timur>
2021-08-20 17:35:09 +0300

    creating changes in styles and making some functions

--- js/index.js
+++ js/index.js
@@ -1,35 +1,48 @@
 'use strict';
 $(document).ready(() => {
     const $btnHamburger = $('.hamburger');
+    const $navItems = document.querySelectorAll('.header__nav-list .nav__list-item');
 
-    $('.user-basket').click((event) => {
-        event.preventDefault();
+    // проверка наличия выпадающего списка для скрытия пиктограммы стрелки
+    $navItems.forEach(nodeItem => {
+        if (nodeItem.querySelector('.nav__link + .nav__list--inner')) {
+            nodeItem.querySelector('.nav__link').classList.add('arrow');
+        };
     });
 
-    // перемещение блока 
+
     if (window.innerWidth <= 758) {
         const $hamburgerNab = $('#hamburgerNav').clone();
         $('#hamburgerNav').remove();
         $('.nav.header__nav').prepend($hamburgerNab);
     };
 
-    $('body').click(() => {
-        $btnHamburger.removeClass('active');
-        $('#hamburgerNav').removeClass('active');
-        $('.header__nav').removeClass('active');
-        if (window.innerWidth <= 758) {
-            if (document.querySelector('.hamburger').classList.contains('active')) {
-                console.log('120');
-                $('body').addClass('ov-hidden');
-            };
-        };
-    });
+
     $btnHamburger.click((elem, classN = 'active') => {
         event.stopPropagation();
-        $btnHamburger.toggleClass(classN);
-        $('#hamburgerNav').toggleClass(classN);
-        $('.header__nav').toggleClass(classN);
+        $btnHamburger.addClass(classN);
+        $('#hamburgerNav').addClass(classN);
+        $('.header__nav').addClass(classN);
+
+        if (window.innerWidth <= 758) {
+            if ($('.hamburger').hasClass('active')) {
+                $('body').addClass('ov-hidden');
+            } else {
+                $('body').removeClass('ov-hidden');
+                $btnHamburger.removeClass('active');
+                $('#hamburgerNav').removeClass('active');
+                $('.header__nav').removeClass('active');
+            }
+            console.log('120');
+        };
     });
+    $(window).click(() => {
+        $btnHamburger.removeClass('active');
+        $('body').removeClass('ov-hidden');
+        $('#hamburgerNav').removeClass('active');
+        $('.header__nav').removeClass('active');
+    });
+
     if ($('.top-banner')) {
         const setDots = () => $(".top-banner .owl-dots").removeClass('disabled');
         $('.top-banner').owlCarousel({
@@ -44,6 +57,13 @@ $(document).ready(() => {
             onChanged: setDots
         });
     };
+    if ($('.b-choice')) {
+        $('.b-choice').owlCarousel({
+            loop: true,
+            items: 5,
+            margin: 14
+        });
+    };
     if ($('.review-slider')) {
         const setBtns = () => $(".review-slider .owl-nav").removeClass('disabled');
         $('.review-slider').owlCarousel({
@@ -51,6 +71,7 @@ $(document).ready(() => {
             margin: 10,
             items: 1,
             navText: [],
+            onResize: setBtns,
             onInitialized: setBtns,
             onChanged: setBtns
         });
@@ -59,4 +80,113 @@ $(document).ready(() => {
         e = event.target;
         $(e).closest('.b-collapse').toggleClass('collapsed').children('.b-collapse__body').slideToggle(200);
     });
+
+    if (document.querySelector('.goods--main')) {
+        let $goodsMain = $('.goods--main');
+        let $goodsMainItems = $goodsMain.find('.goods__item');
+        let $itemsToShow = $goodsMain.find('.goods__item').splice(6,3);
+
+        $itemsToShow.forEach(item => {
+            $(item).hide();
+        });
+        
+        if($goodsMainItems.length <= 6) {
+            $('.show-more').hide();
+        };
+        $('.show-more').click((event) => {
+            event.preventDefault();
+            $itemsToShow.forEach(item => {
+                $(item).show();
+            });
+            $('.show-more').hide();
+        });
+    };
+
+
+    if (document.querySelector('#canvas')) {
+
+        const $imgInput = $('[type="file"]')[0];
+        let s;
+        // добавляем изображение на страницу
+        $imgInput.onchange = function (ev) {
+            const f = ev.target.files[0];
+            const fr = new FileReader();
+            const $img = document.createElement('img');
+            $img.alt = '';
+
+            fr.onload = function (ev2) {
+                $img.setAttribute('src', ev2.target.result)
+            };
+
+            fr.readAsDataURL(f);
+            // document.body.append($img);
+            s = $img;
+
+            setTimeout(() => {
+                const $canvas = $('#canvas')[0];
+                const ctx = $canvas.getContext('2d');
+                ctx.drawImage(s, 0, 0, 200, 200);
+
+                const $inputSurname = $('.constructor__input--surname')[0];
+                const $inputComment = $('.constructor__input--comment')[0];
+
+                $inputComment.oninput = () => {
+                    // обнуляем холст
+                    // ctx.clearRect(0, 0, canvas.width, canvas.height);
+                    // задаем нужный щрифт
+                    ctx.font = '  28px times';
+                    // рисуем текст,используя введенное значение
+                    ctx.fillText($inputComment.value, 50, 100);
+                    function demoFromHTML() {
+                        const pdf = new jsPDF('p', 'pt', 'a4');
+                        pdf.addImage(s, "jpeg", 20, 50, 200, 200);
+                        pdf.setFontSize(28);
+                        pdf.addFont('times', 'Arial', 'normal')
+                        pdf.setFont('times');
+                        pdf.text(20, 200, $inputComment.value);
+                        // source can be HTML-formatted string, or a reference
+                        // to an actual DOM element from which the text will be scraped.
+                        let source = $('#canvas')[0];
+
+                        // we support special element handlers. Register them with jQuery-style 
+                        // ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
+                        // There is no support for any other type of selectors 
+                        // (class, of compound) at this time.
+                        let specialElementHandlers = {
+                            // element with id of "bypass" - jQuery style selector
+                            '#bypassme': function (element, renderer) {
+                                // true = "handled elsewhere, bypass text extraction"
+                                return true
+                            }
+                        };
+                        let margins = {
+                            top: 80,
+                            bottom: 60,
+                            left: 40,
+                            width: 522
+                        };
+                        // all coords and widths are in jsPDF instance's declared units
+                        // 'inches' in this case
+                        pdf.fromHTML(
+                            source, // HTML string or DOM elem ref.
+                            margins.left, // x coord
+                            margins.top, { // y coord
+                            'width': margins.width, // max width of content on PDF
+                            'elementHandlers': specialElementHandlers
+                        },
+
+                            function (dispose) {
+                                // dispose: object with X, Y of the last line add to the PDF 
+                                //          this allow the insertion of new lines after html
+                                pdf.save('Test.pdf');
+                            }, margins
+                        );
+                    };
+                    document.querySelector('#canvas').onclick = demoFromHTML;
+
+                };
+            }, 1000);
+
+        };
+    };
 });
